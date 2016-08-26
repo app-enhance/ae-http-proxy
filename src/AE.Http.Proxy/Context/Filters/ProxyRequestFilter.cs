7eb3b563ec6fce28d3b5acfee21834ae34a7c9e3
Wojciech Gadziński <wojciech.gadzinski@medicover.pl>
2016-08-26 14:31:35 +0200

    Add simplified usage of proxy filters

---
+++ src/AE.Http.Proxy/Context/Filters/ProxyRequestFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace AE.Http.Proxy.Context.Filters
+{
+    using AE.Http.Proxy.Abstractions.Context;
+    using AE.Http.Proxy.Abstractions.Context.Filters;
+
+    public abstract class ProxyRequestFilter : IProxyFilter
+    {
+        public abstract void OnRequest(RequestContext requestContext);
+
+        public void OnResponse(ResponseContext responseContext)
+        {
+        }
+    }
+}
---
+++ src/AE.Http.Proxy/Context/Filters/ProxyResponseFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace AE.Http.Proxy.Context.Filters
+{
+    using AE.Http.Proxy.Abstractions.Context;
+    using AE.Http.Proxy.Abstractions.Context.Filters;
+
+    public abstract class ProxyResponseFilter : IProxyFilter
+    {
+        public void OnRequest(RequestContext requestContext)
+        {
+        }
+
+        public abstract void OnResponse(ResponseContext responseContext);
+    }
+}
